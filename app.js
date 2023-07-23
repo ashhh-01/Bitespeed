6d69b778f4361f0305478aa0149a872715227fe8
Ashrith <ashrithmr01@gmail.com>
2023-07-23 22:41:27 +0530

    DB connection post data

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const express= require("express")
 const app=express()
 const bodyParser=require("body-parser")
+let mysql = require('mysql2');
+const path=require("path")
+
+let pool = mysql.createPool({
+  connectionLimit: 10,
+  host: "localhost",
+  user: "root",
+  password:process.env.MYSQLPASSWORD,
+  database:process.env.MYSQLDB
+});
+
+pool.getConnection(function(err, connection) {
+  if (err) {
+    console.error("Error connecting to MySQL:", err.message);
+    process.exit(1); // Exit the application with a non-zero code
+  }
+  console.log("Connected!");
+  // Do database operations using the "connection" object
+  // Release the connection when done: connection.release();
+});
+app.set('view engine', 'ejs');
 
 app.use(express.json())
 app.use(express.static('public'));
@@ -9,14 +30,12 @@ app.use(bodyParser.urlencoded({
     extended: true
  }))
 
-
 app.get("/",(req,res)=>{
     res.render("index")
-    // res.json(find)
 })
 app.post("/identify",(req,res)=>{
-    console.log(req.body)
-    res.json(req.body)    
+    const {email,phoneNumber,createdAt,updateAt}=req.body
+    // const sql = "INSERT INTO bitespeed (email, username) VALUES ('${name}', '${username}')";
 })
 
 app.listen(3000,()=>{
